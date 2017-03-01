87ab94db39cf3c474c07be2d2602d36e4b41b498
ccli8 <ccli8@nuvoton.com>
2017-03-01 13:03:04 +0800

    [NUC472] Support no-XRAM configuration

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
-
 int main() {
     
-    // TO BE EXTENDED
+    while (true) {
+        void *heap = new uint8_t[32];
+        
+        uint32_t heap_ = ((uint32_t) heap) & 0xF0000000;
+        switch (heap_) {
+            case 0x20000000:
+                printf("Heap allocated at 0x%08x (IRAM)\r\n", heap);
+                break;
+                
+            case 0x60000000:
+                printf("Heap allocated at 0x%08x (XRAM)\r\n", heap);
+                break;
+                
+            default:
+                printf("Heap allocated at 0x%08x (ILLEGAL)\r\n", heap);
+        }
+    
+        delete [] heap;
+        heap = NULL;
+    
+        wait(2.0);
+    }
+    
     return 0;
 }
-
